cc5f31093534aacaf1ba9c26eede757685fe03c5
Avinash Sorab <avinashsp93@gmail.com>
2019-07-13 10:15:31 -0400

    final changes

--- csv_file_creator.py
+++ csv_file_creator.py
@@ -4,4 +4,31 @@ with open('contacts.csv', 'w', newline='') as f:
     thewriter = csv.writer(f)
 
     thewriter.writerow(['avinashsp93@gmail.com'])
-    thewriter.writerow(['deepthidesaitwinkle@gmail.com'])
+    thewriter.writerow(['arpitharamesh2012@gmail.com'])
+    
+
+# contacts.csv entries
+
+# arpitha.rani@spaceagelabs.com.sg
+# ananth.subba@spaceagelabs.com.sg 
+# meyamai@spaceagelabs.com.sg 
+# deepak@spaceagelabs.com.sg 
+# leelakrishna@spaceagelabs.com.sg 
+# ysashikumar@spaceagelabs.com.sg 
+# don.pacheco@spaceagelabs.com.sg 
+
+
+
+# organizations.csv entries
+
+# nus
+# plattnera
+# xperanti
+# 1-knot
+# ledbay
+# hitachi-aqua
+# pmps
+# embrio
+# adventure_cove
+# dpstar
+# singapore-food-delights
--- script.py
+++ script.py
@@ -36,88 +36,110 @@ csrf = client.get(url).cookies['csrftoken']
 login_data = dict(username='saladmin', password='Space@GE;labs', csrfmiddlewaretoken=csrf, next='/')
 r = client.post(url, data=login_data, headers=dict(Referer=url))
 
-# Create and open an output file pointer, close it at the end
-out_fp = open("output.json", "w")
 
-# GET request for Devices
-r = client.get("https://api.spaceagelabs.com.sg/v2/Spaceagelabs/devices?limit=100&offset=0")
+with open('organizations.csv') as org_file:
+    org_reader = csv.reader(org_file, delimiter=',')
+    count = 0
+    for row in org_reader:
+        print(row[0])
+        # GET request for Devices
+        r = client.get("https://api.spaceagelabs.com.sg/v2/"+row[0]+"/devices?limit=100&offset=0")
 
-out_fp.write(r.text)
-results = json.loads(r.text)['results']
-count = json.loads(r.text)['count']
-with open('status.csv', 'w', newline='') as f:
-    thewriter = csv.writer(f)
+        # Create and open an output file pointer, close it at the end
+        out_fp = open("output.json", "w", encoding="utf-8")
 
-    thewriter.writerow(['Devices Count', count])
-    thewriter.writerow('\n')
-
-    thewriter.writerow(['Device ID', 'Name', 'Status', 'Last Seen'])
-    for i in results:
-        dt = 'Not Known'
-        if(i['last_seen'] != None):
-            dt = datetime.datetime.fromtimestamp(i['last_seen']).strftime("%A, %B %d, %Y %I:%M:%S")
-        status = 'Inactive'
-        if(i['is_active']):
-            status = 'Active'
-        thewriter.writerow([i['id'], i['device_name'], status, dt])
-    thewriter.writerow('\n')
-    
-# GET request for Devices Alarms
-r = client.get("https://api.spaceagelabs.com.sg/v2/Spaceagelabs/devices-alarms?limit=100&offset=0")
-
-out_fp.write(r.text)
-results = json.loads(r.text)['results']
-count = json.loads(r.text)['count']
-with open('status.csv', 'a', newline='') as f:
-    thewriter = csv.writer(f)
-
-    thewriter.writerow(['Devices Alarms Count', count])
-    thewriter.writerow('\n')
-
-    thewriter.writerow(['Device ID', 'Created At', 'Cleared At'])
-    for i in results:
-        # Javascript date parsed into known format first and then formatted in accordance with the output requirement
-        date_cleared = i['cleared']
-        date_created = printable_date_format(i['created_at'])
-        if(date_cleared):
-            date_cleared = printable_date_format(i['cleared_at'])
-        else:
-            date_cleared = 'Not Known'
-        if(check_date_intervals(i['created_at'])):
-            thewriter.writerow([i['id'], date_created, date_cleared])
-    thewriter.writerow('\n')
-    
-
-r = client.get("https://api.spaceagelabs.com.sg/v2/Spaceagelabs/feeds/alarms/?limit=100&offset=0")
-
-out_fp.write(r.text)
-results = json.loads(r.text)['results']
-count = json.loads(r.text)['count']
-with open('status.csv', 'a', newline='') as f:
-    thewriter = csv.writer(f)
-
-    thewriter.writerow(['Feed Alarms Count', count])
-    thewriter.writerow('\n')
-
-    thewriter.writerow(['Device ID', 'Created At', 'Cleared At'])
-    for i in results:
-        # Javascript date parsed into known format first and then formatted in accordance with the output requirement
-        date_cleared = i['cleared']
-        date_created = printable_date_format(i['created_at'])
+        out_fp.write(r.text)
+        results = json.loads(r.text)['results']
+        count = json.loads(r.text)['count']
+        write_append_flag = 'w'
+        if(count > 0):
+            write_append_flag = 'a'
+        count+=1
         
-        if(date_cleared):
-            date_cleared = printable_date_format(i['cleared_at'])
-        else:
-            date_cleared = 'Not Known'
-        if(check_date_intervals(i['created_at'])):
-            thewriter.writerow([i['id'], date_created, date_cleared])
-    thewriter.writerow('\n')
+        with open('status.csv', write_append_flag , newline='') as f:
+            thewriter = csv.writer(f)
 
-out_fp.close()
+            thewriter.writerow(['Organization Name', row[0]])
+            thewriter.writerow('\n')
 
-s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+            thewriter.writerow(['Devices'])
+            thewriter.writerow(['Name', 'Alias', 'Status', 'Last Seen'])
+            active_count = 0
+            total_count = 0
+            for i in results:
+                total_count+=1
+                dt = 'Not Known'
+                if(i['last_seen'] != None):
+                    dt = datetime.datetime.fromtimestamp(i['last_seen']).strftime("%A, %B %d, %Y %I:%M:%S")
+                status = 'Inactive'
+                if(i['is_active']):
+                    status = 'Active'
+                    active_count+=1
+                thewriter.writerow([i['device_name'], i['alias_name'].encode("utf-8"), status, dt])
+            thewriter.writerow('\n')
+            device_health = 'Unknown'
+            if(total_count != 0):
+                device_health = ((active_count*1.0)/total_count)*100
+            thewriter.writerow(['Devices Health of Organization', device_health])
+            thewriter.writerow('\n')
+            
+        # GET request for Devices Alarms
+        r = client.get("https://api.spaceagelabs.com.sg/v2/"+row[0]+"/devices-alarms?limit=100&offset=0")
+
+        out_fp.write(r.text)
+        results = json.loads(r.text)['results']
+        count = json.loads(r.text)['count']
+        with open('status.csv', 'a', newline='') as f:
+            thewriter = csv.writer(f)
+
+            thewriter.writerow(['Devices Alarms'])
+            thewriter.writerow(['Device Name', 'Created At', 'Cleared At'])
+            for i in results:
+                # Javascript date parsed into known format first and then formatted in accordance with the output requirement
+                date_cleared = i['cleared']
+                date_created = printable_date_format(i['created_at'])
+                if(date_cleared):
+                    date_cleared = printable_date_format(i['cleared_at'])
+                else:
+                    date_cleared = 'Not Known'
+                if(check_date_intervals(i['created_at'])):
+                    thewriter.writerow([i['name'], date_created, date_cleared])
+            thewriter.writerow('\n')
+            
+
+        r = client.get("https://api.spaceagelabs.com.sg/v2/"+row[0]+"/feeds/alarms/?limit=100&offset=0")
+
+        out_fp.write(r.text)
+        results = json.loads(r.text)['results']
+        count = json.loads(r.text)['count']
+        with open('status.csv', 'a', newline='') as f:
+            thewriter = csv.writer(f)
+
+            thewriter.writerow(['Feed Alarms'])
+            thewriter.writerow(['Device Name', 'Created At', 'Cleared At'])
+            for i in results:
+                # Javascript date parsed into known format first and then formatted in accordance with the output requirement
+                date_cleared = i['cleared']
+                date_created = printable_date_format(i['created_at'])
+                
+                if(date_cleared):
+                    date_cleared = printable_date_format(i['cleared_at'])
+                else:
+                    date_cleared = 'Not Known'
+                if(check_date_intervals(i['created_at'])):
+                    thewriter.writerow([i['feed']['device']['device_name'], date_created, date_cleared])
+            thewriter.writerow('\n')
+        
+            thewriter.writerow('\n')
+            thewriter.writerow('\n')
+            thewriter.writerow('\n')
+            thewriter.writerow('\n')
+        
+        out_fp.close()
+
+s = smtplib.SMTP(host='smtp.gmail.com', port=587)
 s.starttls()
-s.login('', '') # email id and password of the sender mail to be typed inside the single quotations respectively
+s.login('spaceagelabstest@gmail.com', 'sal@12345') # email id and password of the sender mail to be typed inside the single quotations respectively
 
 with open('contacts.csv', newline='') as csvfile:
 
@@ -126,7 +148,7 @@ with open('contacts.csv', newline='') as csvfile:
     for row in reader:
         msg = MIMEMultipart()
 
-        msg['From'] = '' # Add email address that sends out the report
+        msg['From'] = 'spaceagelabstest@gmail.com' # Add email address that sends out the report
         msg['To'] = row[0] # Recepients
         msg['Subject'] = 'Device Status Results'
 
@@ -143,7 +165,7 @@ with open('contacts.csv', newline='') as csvfile:
 
         msg.attach(p)
 
-        s.send_message(msg)
+        # s.send_message(msg)
 
         print("Mail sent successfully to " + row[0])
 
